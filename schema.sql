6132bde641d06434bde522f1ac3966ab719acf3b
Aniket Biprojit Chowdhury <8.aniket.chowdhury@gmail.com>
2024-12-28 14:08:41 +0530

    feat: updated schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE projects (
     tracked_time_in_seconds INTEGER NOT NULL
 );
 
+CREATE TABLE time_tracking (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    project_id INTEGER NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+);
